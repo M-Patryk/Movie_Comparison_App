8042a5aea2c96b14be1bab8b33d907a702e5be30
Patryk <47945758+M-Patryk@users.noreply.github.com>
2020-11-23 09:44:43 +0100

    Raw, not-debounced function to get data from input

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
 	const response = await axios.get('http://www.omdbapi.com/', {
 		params : {
 			apikey : '9bea1d29',
-			// s      : 'avengers',
-			i      : 'tt0848228'
+			s      : searchTerm,
+			// i      : 'tt0848228'
 		}
 	});
 	console.log(response.data);
 };
-fetchData();
 
-// /?apikey=9bea1d29&
+let timeoutId;	// Declared for timeout
+const onInput = (event) => { // This is event for EventListener
+	if(timeoutId){	
+		clearTimeout(timeoutId)		//Funkcja resetujaca timer w przypadku kiedy cos wpisujemy
+	}
+	timeoutId = setTimeout(() => {	//Tutaj jest ten event, ktory jest uruchamiany w momencie wykrycia
+		fetchData(event.target.value)	//  przez EventListener
+	}, 1000);
+}
+const input = document.querySelector('input')	//Selecting the input
+input.addEventListener('input', onInput)	// Add event listener that listens for inputs
