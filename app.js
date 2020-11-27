09a6d81df535ce14cc10a53100c952e8e9765315
Patryk <47945758+M-Patryk@users.noreply.github.com>
2020-11-27 19:56:42 +0100

    Added debounce function (it limits how often given function runs)

--- app.js
+++ app.js
@@ -2,21 +2,37 @@ const fetchData = async (searchTerm) => {
 	const response = await axios.get('http://www.omdbapi.com/', {
 		params : {
 			apikey : '9bea1d29',
-			s      : searchTerm,
+			s      : searchTerm
 			// i      : 'tt0848228'
 		}
 	});
 	console.log(response.data);
 };
 
-let timeoutId;	// Declared for timeout
-const onInput = (event) => { // This is event for EventListener
-	if(timeoutId){	
-		clearTimeout(timeoutId)		//Funkcja resetujaca timer w przypadku kiedy cos wpisujemy
-	}
-	timeoutId = setTimeout(() => {	//Tutaj jest ten event, ktory jest uruchamiany w momencie wykrycia
-		fetchData(event.target.value)	//  przez EventListener
-	}, 1000);
-}
-const input = document.querySelector('input')	//Selecting the input
-input.addEventListener('input', onInput)	// Add event listener that listens for inputs
+//	!!! DEBOUNCE OGRANICZA JAK CZESTO onInput JEST WYKONYWANE !!!
+// (func) w tym przypadku to jest z onInput event, a dokladnie ten kawalek ((event) => {fetchData(event.target.value);});
+const debounce = (func, delay = 1000) => {
+	let timeoutId; //It has to be declared to later use it to declare timeout as this var
+	return (...args) => {
+		if (timeoutId) {
+			//Tutaj standardowo - jesli cos napisze i odpali sie timer to ta funkcja
+			clearTimeout(timeoutId); //go zresetuje
+		}
+		timeoutId = setTimeout(() => {
+			//deklaracja timera
+			func.apply(null, args);
+			//Zasada dzialania "func.apply". Dziala tak jakbym napisal func(arg1, arg2, arg3)
+			//Tyle, ze zamiast podawac wszystkie argumenty po kolei to uzywamy apply i samo podaje wszystkie
+			//Wywolaj funkcje, wez wszystkie argumenty i osobno podawaj je jako arugmenty do oryginalnej funkcji
+		}, delay);
+	};
+};
+
+const onInput = event => {
+	// This is event for EventListener
+	//Tutaj jest ten event, ktory jest uruchamiany w momencie wykrycia przez EventListener
+	fetchData(event.target.value);
+};
+
+const input = document.querySelector('input'); //Selecting the input
+input.addEventListener('input', debounce(onInput, 1000)); // Add event listener that listens for inputs
