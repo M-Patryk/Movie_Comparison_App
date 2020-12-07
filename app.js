efd55c3f07e7777794010c60a8ce92d1d01b9f70
Patryk <47945758+M-Patryk@users.noreply.github.com>
2020-12-07 18:27:13 +0100

    Added two functions #1 One to get clicked movie data #2 Another to make a template to insert data to HTML

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const fetchData = async (searchTerm) => {
 	if (response.data.Error) {
 		return [];
 	}
-
 	return response.data.Search;
 };
 
@@ -56,6 +55,7 @@ const onInput = async (event) => {
 		option.addEventListener('click', () => {
 			dropdown.classList.remove('is-active');
 			input.value = movie.Title;
+			onMovieSelect(movie);
 		});
 
 		resultsWrapper.appendChild(option);
@@ -69,3 +69,32 @@ document.addEventListener('click', (event) => {
 		dropdown.classList.remove('is-active');
 	}
 });
+
+const onMovieSelect = async (movie) => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params : {
+			apikey : '9bea1d29',
+			i      : movie.imdbID
+		}
+	});
+
+	document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = (movieDetails) => {
+	return `
+	<article class = "media">
+		<figure class = "media-left">
+			<p class = "image">
+				<img src = "${movieDetails.Poster}"
+			</p>
+		</figure>
+		<div class = "media-content> 
+			<div class = "content">
+				<h1>${movieDetails.Title}</h1>
+				<h4>${movieDetails.Genre}</h4>
+				<p>${movieDetails.Plot}</p>
+			 </div>
+		</div>
+	`;
+};
