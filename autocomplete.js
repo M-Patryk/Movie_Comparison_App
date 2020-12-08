4f5b2ff8f0bf4b4f9ff5812de43a5378a8df0012
Patryk <47945758+M-Patryk@users.noreply.github.com>
2020-12-08 18:21:04 +0100

    Algorithms mvd from app.js to make it reusable

---
+++ autocomplete.js
@@ -0,0 +1,56 @@
+const createAutoComplete = ({ root }) => {
+	root.innerHTML = `
+	<label><b>Search for a Movie</b></label>
+	<input class='input' />
+	<div class = 'dropdown'>
+		<div class = 'dropdown-menu'>
+			<div class = 'dropdown-content results'></div>
+		</div>
+	</div>
+`;
+
+	const input = root.querySelector('input'); //Selecting the input
+	const dropdown = root.querySelector('.dropdown'); //To jest od autocomplete
+	const resultsWrapper = root.querySelector('.results'); //To jest od autocomplete
+
+	const onInput = async (event) => {
+		// This is event for EventListener
+		//Tutaj jest ten event, ktory jest uruchamiany w momencie wykrycia przez EventListener
+		const movies = await fetchData(event.target.value);
+
+		if (!movies.length) {
+			// Algo to hide dropdown if something was removed from it and its empty
+			dropdown.classList.remove('is-active');
+			return;
+		}
+
+		resultsWrapper.innerHTML = ''; // To clearuje liste filmow z autocomplete
+		dropdown.classList.add('is-active');
+		for (let movie of movies) {
+			const option = document.createElement('a');
+
+			const checkForImgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+			option.classList.add('dropdown-item');
+			option.innerHTML = `
+			<img src="${checkForImgSrc}" />
+			${movie.Title}
+		`;
+
+			option.addEventListener('click', () => {
+				dropdown.classList.remove('is-active');
+				input.value = movie.Title;
+				onMovieSelect(movie);
+			});
+
+			resultsWrapper.appendChild(option);
+		}
+	};
+
+	input.addEventListener('input', debounce(onInput, 500)); // Add event listener that listens for inputs
+
+	document.addEventListener('click', (event) => {
+		if (!root.contains(event.target)) {
+			dropdown.classList.remove('is-active');
+		}
+	});
+};
